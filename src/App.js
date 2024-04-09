cbc73cc39008cfb0d8c97b0be6c82fc1f66f6e62
Yukti Guru <126247680+yuktiguru11@users.noreply.github.com>
2024-04-09 22:21:38 +0530

    Revert "Login and signup page addition"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { BrowserRouter, Routes } from "react-router-dom";
 import Login from "./feature/login";
 
 
 function App() {
   return (
     <>
-    <Routes>
-      <Switch>
-      <Route exact path="/" component={Login}/>
-      <Route path="/signup"  component={Signup}/>
-      </Switch>
     <Login/>
-    </Routes>
     </>
   );
 }
